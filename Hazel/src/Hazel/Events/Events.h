e5c8855b8de4d686582fbd72bab8e3e326239589
Ireneusz Florek <irekflorek@gmail.com>
2020-12-23 17:36:58 +0100

    Events started

---
+++ Hazel/src/Hazel/Events/Events.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "Hazel/Core.h"
+
+
+
+namespace Hazel
+{
+	enum class EventType
+	{
+		None = 0,
+		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+		KeyPressed, KeyReleased,
+		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+	};
+
+	enum EventCategory
+	{
+		None = 0,
+		EventCategoryApplication = BIT(0),
+		EventCategoryInput       = BIT(1),
+		EventCategoryKeyboard    = BIT(2),
+		EventCategoryMouse       = BIT(3),
+
+	};
+	class Event
+	{
+	public:
+		Event() {};
+		virtual ~Event() {};
+
+		virtual EventType GetEventType() const = 0;
+		virtual const char* GetName() const = 0;
+
+	private:
+	};
+
+
+	class KeyPress : public Event
+	{
+
+	};
+}
